0e3ecaae227b031460f4dfd0d61cac3886a9b4f6
Alejandro Flores <alejandro@esporaestudio.com>
2018-09-06 10:53:23 -0500

    Se agregó el ejemplo del manejo de eventos de animación

---
+++ animation-basics/Assets/Scripts/PlayerEventHandler.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEventHandler : MonoBehaviour {
+    void OnAnimationDeadEnd() {
+        Debug.LogError("OnAnimationDeadEnd()");
+    }
+}
