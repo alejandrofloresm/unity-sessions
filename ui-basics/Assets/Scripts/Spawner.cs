86f552e92c14faf5a198889a3f3cd9130f58e496
Alejandro Flores <alejandro@esporaestudio.com>
2018-09-20 13:56:28 -0500

    UI Basics: Creación del hongo a través del click del botón

---
+++ ui-basics/Assets/Scripts/Spawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject ObjectToSpawn;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            DoSpawn();
+        }
+	}
+
+    public void DoSpawn() {
+        GameObject go = Instantiate(ObjectToSpawn,
+            transform.position,
+            Quaternion.identity);
+        go.AddComponent<Rigidbody>();
+        go.AddComponent<BoxCollider>();
+    }
+}
